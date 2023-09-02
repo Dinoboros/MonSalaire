b086db7737454c3fb0053cca8d3432a0e23c7c60
Dinoboros <meryl.valier@gmail.com>
2023-09-03 01:09:53 +0200

    Initial Commit

---
+++ MonSalaire/MonSalaireApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonSalaireApp.swift
+//  MonSalaire
+//
+//  Created by Méryl VALIER on 03/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct MonSalaireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
